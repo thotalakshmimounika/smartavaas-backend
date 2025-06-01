8103b2921a4e793a8049184638b6a647bcc3c6d4
BaaruTech1806 <134836941+BaaruTech1806@users.noreply.github.com>
2025-06-01 17:23:20 +0530

    Update WebConfig.java

--- src/main/java/com/smartavaas/config/WebConfig.java
+++ src/main/java/com/smartavaas/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://develop--adorable-liger-d685f6.netlify.app", "http://localhost:4200", "http://frontend-219121912.us-east-1.elb.amazonaws.com")
+                .allowedOrigins("https://develop--adorable-liger-d685f6.netlify.app", "http://localhost:4200", "http://frontend-219121912.us-east-1.elb.amazonaws.com", "http://3.231.160.198")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
