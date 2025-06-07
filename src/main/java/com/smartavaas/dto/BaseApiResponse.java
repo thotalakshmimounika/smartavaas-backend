73d0a9ec1ecf15ce78e2013b49b58cd73a737c23
kahar12911 <kahar12911@gmail.com>
2025-06-07 23:21:10 +0530

    Added Check-Email for sign-in without password option and Added Common BaseAPiResponse

--- src/main/java/com/smartavaas/dto/BaseApiResponse.java
+++ src/main/java/com/smartavaas/dto/BaseApiResponse.java
@@ -4,14 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Data
 @Builder
-public class OtpVerifyResponse {
+public class BaseApiResponse<T> {
     private LocalDateTime timestamp;
     private int statusCode;
-    private String status;        // "success", "fail", "error"
+    private String status;   // "success", "fail", "error"
     private String message;
-    private Map<String, Object> data; // Can include JWT, email, role, etc.
+    private T data;
 }
---
+++ src/main/java/com/smartavaas/dto/CheckEmailRequest.java
@@ -0,0 +1,8 @@
+package com.smartavaas.dto;
+
+import lombok.Data;
+
+@Data
+public class CheckEmailRequest {
+    private String email;
+}
