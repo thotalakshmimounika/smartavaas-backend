349fb45d977fe88d8d7366f47d4855cfda6f1720
thotalakshmim <LakshmiMounika.Thota@garmin.com>
2025-06-15 15:54:32 +0530

    Updated the code delete and patch request

--- src/main/java/com/smartavaas/controller/MaintenanceController.java
+++ src/main/java/com/smartavaas/controller/MaintenanceController.java
@@ -1,7 +1,6 @@
 package com.smartavaas.controller;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
